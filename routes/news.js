1a3e6fe24a43876f9caf018e5502e8c7acdc3267
yasmin <zhamborova.y@gmail.com>
2020-11-06 11:59:17 +0300

    news api

---
+++ routes/news.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('5f1246674c184e60bb9e88f19186b8b3');
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  newsapi.v2.everything({
+    q: 'environment',
+    from: '2020-10-10',
+    language: 'en',
+    sortBy: 'relevancy',
+  }).then(response => {
+    console.log(response);
+    res.send(response);
+  });
+});
+
+/* GET users listing. */
+router.get('/:title', function(req, res, next) {
+  newsapi.v2.everything({
+    q: req.params.title,
+    from: '2020-10-10',
+    language: 'en',
+    sortBy: 'relevancy',
+  }).then(response => {
+    console.log(response);
+    res.send(response);
+  });
+});
+
+
+
+
+module.exports = router;
