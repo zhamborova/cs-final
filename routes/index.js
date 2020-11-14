48fd547bd51080e439eb75ec7fbf40e61a8a072a
yasmin <zhamborova.y@gmail.com>
2020-11-14 15:18:36 +0300

    added cors

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors')
 
+
+express.use(cors())
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
