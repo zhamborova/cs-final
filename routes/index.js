67eaea0cb3d7c54196bd99c6466f6171797f761b
yasmin <zhamborova.y@gmail.com>
2020-11-14 15:21:52 +0300

    fixed cors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var cors = require('cors')
+var app = express()
+
+app.use(cors())
 
 
-express.use(cors())
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
