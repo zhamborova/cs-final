6c21cacd8912c42eeb03bc141ec77050ed9e8642
yasmin <zhamborova.y@gmail.com>
2020-12-12 00:10:16 +0300

    fixed newsapi

--- routes/news.js
+++ routes/news.js
@@ -3,14 +3,10 @@ var router = express.Router();
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('5f1246674c184e60bb9e88f19186b8b3');
 
-new Date().getDate()
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  let date = new Date();
+
   newsapi.v2.everything({
-    q: 'environment',
-    from: `${date.getFullYear()}-${date.getMonth()}-${date.getDay() - 10}`,
+    q: 'waste',
     language: 'en',
     sortBy: 'relevancy',
   }).then(response => {
@@ -21,10 +17,11 @@ router.get('/', function(req, res, next) {
 
 /* GET users listing. */
 router.get('/:title', function(req, res, next) {
-
+  let date = new Date();
+  console.log(date)
   newsapi.v2.everything({
     q: req.params.title,
-    from: `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`,
+
     language: 'en',
     sortBy: 'relevancy',
   }).then(response => {
